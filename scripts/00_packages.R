640842e831d242fcc6883557d2904ae2adb85020
Randall Hidalgo <randall.hsanchez@gmail.com>
2026-06-26 20:20:22 -0600

    updated packages used

--- scripts/00_packages.R
+++ scripts/00_packages.R
@@ -1,4 +1,6 @@
 # Este script contiene las librerias o paquetes que se van a 
 # utilizar a lo largo del poryecto
 
-library(tidyverse) # v2.0.0
+library(tidyverse)  # v2.0.0
+library(skimr)      # v2.1.5
+library(janitor)    # v2.2.1
