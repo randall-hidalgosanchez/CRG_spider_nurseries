53ee3daf3c241eb10e53b0707ae13cf651c1b639
Randall Hidalgo <randall.hsanchez@gmail.com>
2026-06-27 22:31:20 -0600

    paquetes actualizados

--- scripts/00_packages.R
+++ scripts/00_packages.R
@@ -4,3 +4,9 @@
 library(tidyverse)  # v2.0.0
 library(skimr)      # v2.1.5
 library(janitor)    # v2.2.1
+library(glmmTMB)    # v1.1.11
+library(mgcv)       # v1.9-3
+library(DHARMa)     # v0.4.7
+library(sjPlot)     # v2.8.17
+library(emmeans)    # v1.11.0
+library(lme4)       # v1.1-36
